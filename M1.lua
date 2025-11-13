2aee2c9a7c223d703e700e881516e4dcadd1e1be
Miller-He <azbcd2993@gmail.com>
2025-11-13 13:54:14 +0700

    Add files via upload

--- M1.lua
+++ M1.lua
@@ -1,6 +1,7 @@
 Arch = gg.getTargetInfo()
 local p_size = Arch.x64 and 0x8 or 0x4
 local p_type = Arch.x64 and 32 or 4
+gg.clearResults()
 -- ===================================================================
 local Region = {
     ["Ch"] = gg.REGION_C_HEAP,
@@ -11,6 +12,18 @@ local Region = {
     ['O']  = gg.REGION_OTHER,
     ['Xa'] = gg.REGION_CODE_APP,
 }
-gg.setRanges(Region["Ca"], Region["A"], Region["O"])
+gg.setRanges(Region["Ca"] | Region["A"] | Region["O"] | Region["Cd"] | Region["Xa"])
 member = "get_price_type"
 gg.searchNumber(string.format("Q 00 '%s' 00", member), gg.TYPE_BYTE)
+local a = gg.getResults(gg.getResultsCount((gg.refineNumber(member:byte(), 1))))
+
+gg.clearResults()
+
+for i,v in ipairs(a) do
+    gg.searchNumber(v.address, p_type)
+    if gg.getResultsCount() > 0 then
+        v.name = "This is it"
+        gg.addListItems({v})
+        gg.clearResults()
+    end
+end
